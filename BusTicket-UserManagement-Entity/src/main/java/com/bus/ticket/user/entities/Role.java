46bf1a37436eaa0fbaac0bf593253874f7cbb349
VinodHoney Kusuma <VinodHoney Kusuma@PC>
2018-07-27 23:38:06 +0530

    intial check in for user and role entities

---
+++ BusTicket-UserManagement-Entity/src/main/java/com/bus/ticket/user/entities/Role.java
@@ -0,0 +1,76 @@
+package com.bus.ticket.user.entities;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+
+/**
+ * The persistent class for the roles database table.
+ * 
+ */
+@Entity
+@Table(name="roles")
+@NamedQuery(name="Role.findAll", query="SELECT r FROM Role r")
+public class Role implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+
+	private String role;
+
+	//bi-directional many-to-one association to User
+	@OneToMany(mappedBy="role")
+	private List<User> users;
+
+	public Role() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getRole() {
+		return this.role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public List<User> getUsers() {
+		return this.users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+
+	public User addUser(User user) {
+		getUsers().add(user);
+		user.setRole(this);
+
+		return user;
+	}
+
+	public User removeUser(User user) {
+		getUsers().remove(user);
+		user.setRole(null);
+
+		return user;
+	}
+
+}
---
+++ BusTicket-UserManagement-Entity/src/main/java/com/bus/ticket/user/entities/User.java
@@ -0,0 +1,147 @@
+package com.bus.ticket.user.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
+
+
+
+/**
+ * The persistent class for the user database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="User.findAll", query="SELECT u FROM User u")
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+
+	@Column(name="b_travels")
+	private String bTravels;
+
+	@Column(name="c_password")
+	private String cPassword;
+
+	private String email;
+
+	@Column(name="first_name")
+	private String firstName;
+
+	@Column(name="last_name")
+	private String lastName;
+
+	@Column(name="middle_name")
+	private String middleName;
+
+	private String mobile;
+
+	private String password;
+
+	private String username;
+
+	//bi-directional many-to-one association to Role
+	@ManyToOne(fetch=FetchType.LAZY)
+	private Role role;
+
+	public User() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getBTravels() {
+		return this.bTravels;
+	}
+
+	public void setBTravels(String bTravels) {
+		this.bTravels = bTravels;
+	}
+
+	public String getCPassword() {
+		return this.cPassword;
+	}
+
+	public void setCPassword(String cPassword) {
+		this.cPassword = cPassword;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getMiddleName() {
+		return this.middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getMobile() {
+		return this.mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Role getRole() {
+		return this.role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+}
